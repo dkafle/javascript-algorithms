5290722c8fb19e7466ec509f7e4b5a99853e0712
Deepak <dkafle@gmail.com>
2016-03-07 22:52:45 -0800

    basic tree structure created

---
+++ tree/tree1.js
@@ -0,0 +1,12 @@
+var Node = function (data, leftNode, rightNode) {
+  this.data = data;
+  this.leftNode = leftNode;
+  this.rightNode = rightNode;
+};
+
+var Tree = function () {
+  var count = 0;
+  var root = null;
+};
+
+
